9bfcf5c257e13c68bf5a272541a12c708e79f140
dreackdown <hugofaria157@live.com>
2023-10-03 09:31:07 -0300

    criacao do job e step

---
+++ src/main/java/dev/hugofaria/backend/entity/Transacao.java
@@ -0,0 +1,42 @@
+package dev.hugofaria.backend.entity;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+
+public record Transacao(
+        Long id,
+        Integer tipo,
+        Date data,
+        BigDecimal valor,
+        Long cpf,
+        String cartao,
+        Time hora,
+        String donoDaLoja,
+        String nomeDaLoja) {
+
+    // Wither Pattern
+//    public Transacao withValor(BigDecimal valor) {
+//        return new Transacao(
+//                id, tipo, data, valor, cpf,
+//                cartao, hora, donoDaLoja, nomeDaLoja);
+//    }
+//
+//    public Transacao withData(String data) throws ParseException {
+//        var dateFormat = new SimpleDateFormat("yyyyMMdd");
+//        var date = dateFormat.parse(data);
+//
+//        return new Transacao(
+//                id, tipo, new Date(date.getTime()), valor, cpf,
+//                cartao, hora, donoDaLoja, nomeDaLoja);
+//    }
+//
+//    public Transacao withHora(String hora) throws ParseException {
+//        var dateFormat = new SimpleDateFormat("HHmmss");
+//        var date = dateFormat.parse(hora);
+//
+//        return new Transacao(
+//                id, tipo, data, valor, cpf,
+//                cartao, new Time(date.getTime()), donoDaLoja, nomeDaLoja);
+//    }
+}
